e7774472409a1a71238aed6e391ed1a45f83b1a7
Lewis Wilkins <lewiswilkins@me.com>
2018-10-08 11:38:10 +0200

    Tidied up code and added acceptance and efficiency

--- helpers.py
+++ helpers.py
@@ -32,3 +32,42 @@ def _remove_overflow(hist, err):
         else:
             raise ValueError('err has wrong shape')
     return hist, err
+
+
+def percentage_difference(a, b):
+    return [((i-j)/i)*100 for i, j in zip(a,b)]
+
+def folding(truth, migration, efficiency, acceptance):
+    reco_folded = []
+    for j in range(0,2):
+        summation = 0;
+        for i,t in enumerate(truth):
+            summation += migration[i,j] * efficiency[i] * t
+        reco_folded.append(summation * 1/acceptance[j])
+
+    return reco_folded
+
+def compare_method(truth_1, M_1, truth_2, M_2, acceptance_1=None, efficiency_1=None,
+                   acceptance_2=None, efficiency_2=None):
+    if not acceptance_1:
+        reco_1 = np.matmul(M_1, truth_1)
+        reco_2 = np.matmul(M_2, truth_2)
+        reco_2_folded = np.matmul(M_1, truth_2)
+    else:
+        reco_1 = folding(truth_1, M_1, efficiency_1, acceptance_1)
+        reco_2 = folding(truth_2, M_2, efficiency_2, acceptance_2)
+        reco_2_folded = folding(truth_2, M_1, efficiency_1, acceptance_1)
+    TF_1 = reco_1/truth_1
+    print("Our truth_1 = %s and our reco_1 = %s" % (str(truth_1), str(reco_1)))
+    print("The migration matrix M_1 = ")
+    print(np.matrix(M_1))
+    print("The transfer function TF_1 = %s" % str(TF_1))
+    TF_2 = reco_2/truth_2
+    print("Our truth_2 = %s and our reco_2 = %s" % (str(truth_2), str(reco_2)))
+    print("The migration matrix M_2 = ")
+    print(np.matrix(M_2))
+    print("The transfer function TF_2 = %s" % str(TF_2))
+    reco_2_transfer = truth_2 * TF_1
+    print(reco_2_transfer)
+    print("Difference between folded and actual reco_2 = %s %%" % str(percentage_difference(reco_2, reco_2_folded)))
+    print("Difference between transfer function and actual reco_2 = %s %%" % str(percentage_difference(reco_2, reco_2_transfer)))
